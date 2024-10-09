025f61befd6ae4dce925e31039d29c913d98c617
Nick Karpenske <randland@gmail.com>
2024-10-09 15:03:11 -0600

    Remove initial admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
+    # Add dashboard for your models here
     resources :recipes
+
+    root to: "recipes#index" # <--- Root route
   end
 
   resources :recipes, only: %i[show]
